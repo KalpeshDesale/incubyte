2b068a5edb0e2b23e8f76d286061758b0cc54fa6
KalpeshDesale <kalpeshdesale0499@gmail.com>
2021-10-18 01:17:23 +0530

    test 3

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -2,15 +2,15 @@ package calculator;
 
 class StringCalculator {
 
-   public int add(String numbers ) {
+	public int add(String numbers ) {
 		String str=numbers;
-		StringTokenizer st = new StringTokenizer(str, ",");
+		StringTokenizer st = new StringTokenizer(str, ",\\n");
 		
 		int sum=0;
 		while(st.hasMoreTokens()) {
 			sum+= Integer.parseInt((String) st.nextElement());
 		}	
 		return sum;
-	}  
+	} 
 
 }
